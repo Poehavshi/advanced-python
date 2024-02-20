94b503ef440df296bd58306c66519b36f111cc45
arkadiysotnikov <cotnikoarkady@gmail.com>
2024-02-20 19:53:53 +0400

    feat(cli): implement tail utility

--- homework-1/filescli/main.py
+++ homework-1/filescli/main.py
@@ -20,8 +20,33 @@ def number_lines(file):
     if file.name == "-":
         file = stdin
     for i, line in enumerate(file):
-        print(f"{i+1} {line}", end="")
+        print(f"\t{i+1}	{line}", end="")
     file.close()
+
+
+# specify multiple files as arguments
+@main.command("tail")
+@click.argument("files", type=click.File(), nargs=-1)
+@click.option(
+    "-n",
+    type=click.INT,
+    default=10,
+    help="output the last N lines, instead of the last 10, if we have input from stdin,"
+    "we will output the last 17 lines.",
+)
+def tail(files, n):
+    need_to_print_file_name = len(files) > 1
+    for file in files:
+        print(f"==> {file.name} <==") if need_to_print_file_name else None
+        lines = file.readlines()
+        for line in lines[-n:]:
+            print(line, end="")
+        file.close()
+    if len(files) == 0:
+        n = 17 if n == 10 else n
+        lines = stdin.readlines()
+        for line in lines[-n:]:
+            print(line, end="")
 
 
 if __name__ == "__main__":
