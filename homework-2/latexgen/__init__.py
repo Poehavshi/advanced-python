d78a995fc87569a98a8bd7fa705b4cf327ce7247
Sotnikov Arkadiy <cotnikoarkady@gmail.com>
2024-03-19 12:50:15 +0400

    feat(distribution): add dockerfile and docker-compose

---
+++ homework-2/latexgen/__init__.py
@@ -0,0 +1,1 @@
+from .latex_elements_generation import generate_table, generate_document, generate_image
--- homework-2/main.py
+++ homework-2/main.py
@@ -1,3 +1,5 @@
+import os
+
 from latexgen.latex_elements_generation import generate_table, generate_document, generate_image
 
 
@@ -9,11 +11,20 @@ def task1(output_dir="artifacts", file_name="output.tex"):
 
 
 def task2(output_dir="artifacts", file_name="output.tex", image_path="../data/image.png"):
-    result = generate_document(nested_code=generate_image(image_path, 100, 100), title="My Image", author="Me", date="Today")
+    table = [["1", "2", "3"], ["4", "5", "6"]]
+    nested_code = generate_table(table=table) + generate_image(image_path, 100, 100)
+    result = generate_document(nested_code=nested_code, title="My Image", author="Me", date="Today")
+    print(result)
     with open(f'{output_dir}/{file_name}', 'w') as f:
         f.write(result)
 
 
 if __name__ == '__main__':
-    task1("artifacts", "task2.tex")
-    task2("artifacts", "task2.tex", "../data/input.png")
+    print("Running tasks...")
+    os.makedirs("artifacts", exist_ok=True)
+    print(os.listdir("."))
+    print(os.listdir("artifacts"))
+    task1("artifacts", "task1.tex")
+    task2("artifacts", "task2.tex", "input.png")
+    os.system("pdflatex -output-directory=artifacts artifacts/task2.tex")
+    print("Tasks completed.")
